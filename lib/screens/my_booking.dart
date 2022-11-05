1dfc3fbb1a112555a4bec39a95a1c2a35686db24
codcsd191f-019 <88055116+IT20668686@users.noreply.github.com>
2022-11-05 12:02:56 +0530

    my booking edit

--- lib/screens/my_booking.dart
+++ lib/screens/my_booking.dart
@@ -2,7 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:ticketingsystem/styles/constants.dart';
 import '../model/bookingModel.dart';
 import '../model/user_model.dart';
 
@@ -65,16 +67,29 @@ class _MyBookingState extends State<MyBooking> {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: HexColor('#00FFFF').withOpacity(0.4),
       body: Column(
         children: [
+          Padding(
+            padding:
+                EdgeInsets.only(top: height * 0.10, bottom: height * 0.025),
+            child: Text(
+              'My Bookings',
+              style: GoogleFonts.dmSans(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+                color: kWhite,
+              ),
+            ),
+          ),
           SingleChildScrollView(
             child: Column(
               //chip words
               children: <Widget>[
                 const SizedBox(height: 10),
                 SizedBox(
-                    width: width * 0.94,
-                    height: height * 0.90,
+                    width: width,
+                    height: height * 0.70,
                     child: FutureBuilder<List<Bookings>>(
                         future: fetchRecords(),
                         builder: (context, snapshot) {
@@ -87,36 +102,76 @@ class _MyBookingState extends State<MyBooking> {
                                 itemCount: data.length,
                                 itemBuilder: (context, index) {
                                   return (SizedBox(
-                                    height: 100,
+                                    height: 50,
                                     child: Column(
                                       children: <Widget>[
-                                        // Row(
-                                        //   children: <Widget>[
-                                        //     Padding(
-                                        //       padding:
-                                        //           const EdgeInsets.all(8.0),
-                                        //       child: Text(data[index].From,
-                                        //           style: TextStyle(
-                                        //             color: Colors.blue,
-                                        //             fontWeight: FontWeight.w900,
-                                        //           )),
-                                        //     ),
-                                        //     Text(data[index].To),
-                                        //     Text(data[index].Date +
-                                        //         ' ' +
-                                        //         data[index].Time),
-                                        //     Text(data[index].Amount),
-                                        //   ],
-                                        // ),
-                                        ListTile(
-                                          leading: Text(
-                                              'From : ' + data[index].From),
-                                          title: Text(data[index].To),
-                                          subtitle: Text(data[index].Date +
-                                              ' ' +
-                                              data[index].Time),
-                                          trailing: Text(data[index].Amount),
-                                        )
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Row(
+                                            children: <Widget>[
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.all(7.0),
+                                                child: Text(
+                                                  data[index].From,
+                                                  style: GoogleFonts.dmSans(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 14,
+                                                    color: HexColor('#abf7b1')
+                                                        .withOpacity(0.4),
+                                                  ),
+                                                ),
+                                              ),
+                                              Padding(
+                                                  padding:
+                                                      const EdgeInsets.all(7.0),
+                                                  child: Text(
+                                                    data[index].To,
+                                                    style: GoogleFonts.dmSans(
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      fontSize: 14,
+                                                      color: HexColor('#abf7b1')
+                                                          .withOpacity(0.4),
+                                                    ),
+                                                  )),
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.all(7.0),
+                                                child: Text(
+                                                  data[index].Date +
+                                                      ' ' +
+                                                      data[index].Time,
+                                                  style: GoogleFonts.dmSans(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 14,
+                                                    color: HexColor('#abf7b1')
+                                                        .withOpacity(0.4),
+                                                  ),
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.all(7.0),
+                                                child: Text(
+                                                  data[index].Amount,
+                                                  style: GoogleFonts.dmSans(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 14,
+                                                    color: HexColor('#abf7b1')
+                                                        .withOpacity(0.4),
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        // ListTile(
+                                        //   leading: Text(data[index].From,style: TextStyle(color: Colors.white)),
+                                        //   title: Text(data[index].To),
+                                        //   trailing: Text(data[index].Amount),
+                                        // )
+                                       
                                       ],
                                     ),
                                   ));
